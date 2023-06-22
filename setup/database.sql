c1f256ac9de043967d56a1e3110a1d231626b530
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2023-06-22 20:51:52 +0800

    perf: use `utf8_general_ci` collate

--- setup/database.sql
+++ setup/database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jun 11, 2023 at 10:15 AM
+-- Generation Time: Jun 22, 2023 at 12:46 PM
 -- Server version: 8.0.27
 -- PHP Version: 7.4.26
 
@@ -30,13 +30,13 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `accounts`;
 CREATE TABLE IF NOT EXISTS `accounts` (
   `id` int NOT NULL AUTO_INCREMENT COMMENT 'Account ID',
-  `name` varchar(255) NOT NULL COMMENT 'Account full name',
-  `studentid` varchar(64) NOT NULL COMMENT 'Student ID',
-  `username` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'Account username',
-  `password` varchar(72) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'Account hashed password',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'Account full name',
+  `studentid` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'Student ID',
+  `username` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'Account username',
+  `password` varchar(72) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'Account hashed password',
   `added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Account added date',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
@@ -47,13 +47,13 @@ CREATE TABLE IF NOT EXISTS `accounts` (
 DROP TABLE IF EXISTS `calendar`;
 CREATE TABLE IF NOT EXISTS `calendar` (
   `id` int NOT NULL AUTO_INCREMENT COMMENT 'Event ID',
-  `name` varchar(255) NOT NULL COMMENT 'Event Name',
-  `description` text NOT NULL COMMENT 'Event Description',
-  `start` varchar(16) NOT NULL COMMENT 'Event start date',
-  `end` varchar(16) NOT NULL COMMENT 'Event end date',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'Event Name',
+  `description` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'Event Description',
+  `start` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'Event start date',
+  `end` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'Event end date',
   `added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Event added date',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
